3afff2749f31ca805e15fddbf6ae1a3824a1f9dc
FrancoNicolasRomero <romerofranco.168@gmail.com>
2025-08-08 00:25:22 -0300

    Capturando-Input

--- app.js
+++ app.js
@@ -1,1 +1,10 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+
+// Inicia declarando una variable de tipo array, que almacenara el nombre  de los amigos ingresados
+let amigos = [];
+
+//Capturar el valor del campo de entrada
+function agregarAmigo() {
+  let nombreAmigos = document.getElementById("amigo").value;
+  console.log(nombreAmigos);
+}
