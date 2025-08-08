d34e2e7e5372df08c099d0fab670c3300d30ef8e
FrancoNicolasRomero <romerofranco.168@gmail.com>
2025-08-08 01:27:29 -0300

    Agregando-nombres-y-limpiando-caja

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
 
 // Inicia declarando una variable de tipo array, que almacenara el nombre  de los amigos ingresados
-let amigos = [];
+let listaAmigos = [];
 
 //Capturar el valor del campo de entrada y alerta caja vacia
 function agregarAmigo() {
@@ -9,8 +9,13 @@ function agregarAmigo() {
   if (valorCaja.value === "") {
     alert("Por favor, inserte un nombre");
   } else {
+    //Agregamos los nombres al array
     let nombreAmigos = document.getElementById("amigo").value;
-    console.log(nombreAmigos);
+    listaAmigos.push(nombreAmigos);
+    //Limpiamos la caja
+    document.getElementById("amigo").value = "";
+    console.log(listaAmigos);
   }
 }
 
+//Actualizar el array de amigos, si el valor es valido añadirlo a la variable amigos con el metodo .push
